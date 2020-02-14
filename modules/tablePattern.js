f01383de4570fceab501e27c39e7207792f8b861
eugenrobanescu <56713706+eugenrobanescu@users.noreply.github.com>
2020-02-14 15:48:17 +0200

    q

---
+++ modules/tablePattern.js
@@ -0,0 +1,14 @@
+const tableAthletes = ` <div id="tableContainer"><table class="dataTable">
+<tr>
+  <th>First Name</th>
+  <th>Last Name</th>
+  <th>Test</th>
+  <th>Result1</th>
+  <th>Result2</th>
+  <th>Result3</th>
+</tr>
+
+{%ATHLETESDATA%}
+</table></div>`;
+
+module.exports = tableAthletes;
